43b43a28a1dd22f4d166b3b34d619a1af912eb27
d2kagw <aaron.wallis@gmail.com>
2015-12-31 17:15:23 +1100

    other hackery

---
+++ tools/udp.py
@@ -0,0 +1,37 @@
+import socket
+
+UDP_IP = "192.168.0.255"
+UDP_PORT = 11647
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+
+
+
+import numpy as np
+import cv2
+import time
+import socket
+
+UDP_IP = "192.168.0.255"
+UDP_PORT = 11647
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+img = np.zeros((100,100,3), np.uint8)
+
+while True:
+  data, addr = sock.recvfrom(12)
+  color = data.split(',')
+  color = np.Scalar(float(color[2]), float(color[1]), float(color[0]))
+  cv2.rectangle(img, (0, 0), (100,100), color, -1, 4 );
+  v = cv2.imshow('image', img)
+  v = cv2.waitKey(1)
